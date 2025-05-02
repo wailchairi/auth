4e6e2bc5a14198ccc96690c98b74107470d06d36
wailchairi <chairiwail@gmail.com>
2025-05-02 16:33:10 +0100

    just fixing a small typo in the user drop down

--- src/components/UserDropdown.jsx
+++ src/components/UserDropdown.jsx
@@ -22,7 +22,7 @@ const UserDropdown = () => {
 
       {isOpen && (
         <div className="absolute right-0 mt-2 w-40 bg-white  rounded shadow z-10">
-          <div className="px-4 py-2 text-gray-700">{user?.name}</div>
+          <div className="px-4 py-2 text-gray-700">{user?.firstName}</div>
           <Link
             to="/profile"
             className="block px-4 py-2 hover:bg-gray-100 text-black"
